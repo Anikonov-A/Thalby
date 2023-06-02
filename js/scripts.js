789c4f349a4ea420b7b1c18c484bb5f6aa28549e
Anton Anikonov <antonanikonov1@gmail.com>
2023-06-02 12:52:37 +0300

    Create project

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        nav:true,
+        autoplay:true,
+        autoplayTimeout:2000,
+        navText: [ '', ' ' ],
+
+        responsive:{
+            0:{
+                items:1
+            },
+          /*  768:{
+                items:2
+            },*/
+            1100:{
+                items:5
+            }
+        }
+    });
+});
